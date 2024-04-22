b80c42b381b31afd8a47454b7df04b93a1da9639
lipinkov <stdvlp888@gmail.com>
2024-04-22 18:24:28 +0300

    Добавление инструментов рисования и цветовой сетки.

--- Paint.py
+++ Paint.py
@@ -5,7 +5,7 @@ Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
-from kivy.graphics import Color, Rectangle
+from kivy.graphics import Color, Rectangle, Line
 
 KV = """
 BoxLayout:
@@ -27,6 +27,7 @@ BoxLayout:
             text: 'Очистить'
             size_hint_x: None
             width: 100
+            on_release: root.ids.paint_widget.clear_canvas()
         Label:
             text: 'Paint'
             color: 0.5, 0, 0.5, 1
@@ -63,6 +64,56 @@ BoxLayout:
                 Rectangle:
                     size: self.size
                     pos: self.pos
+            Button:
+                text: 'Кисть'
+                size_hint_y: None
+                height: 99.68
+                on_release: root.ids.paint_widget.switch_to_brush()
+            Button:
+                text: 'Ластик'
+                size_hint_y: None
+                height: 99.68
+                on_release: root.ids.paint_widget.switch_to_eraser()
+            GridLayout:
+                cols: 2
+                size_hint_y: None
+                height: 600.501
+                Button:
+                    background_normal: ''
+                    background_color: 0.6, 0.2, 0.2, 1  # Темно-красный
+                Button:
+                    background_normal: ''
+                    background_color: 0.2, 0.6, 0.2, 1  # Темно-зеленый
+                Button:
+                    background_normal: ''
+                    background_color: 0.2, 0.2, 0.6, 1  # Темно-синий
+                Button:
+                    background_normal: ''
+                    background_color: 0.6, 0.6, 0.2, 1  # Темно-желтый
+                Button:
+                    background_normal: ''
+                    background_color: 0.6, 0.4, 0, 1  # Темно-оранжевый
+                Button:
+                    background_normal: ''
+                    background_color: 0.4, 0, 0.4, 1  # Темно-фиолетовый
+                Button:
+                    background_normal: ''
+                    background_color: 0, 0.4, 0.4, 1  # Темно-бирюзовый
+                Button:
+                    background_normal: ''
+                    background_color: 0.4, 0.4, 0, 1  # Темно-оливковый
+                Button:
+                    background_normal: ''
+                    background_color: 0.3, 0.3, 0.3, 1  # Серый
+                Button:
+                    background_normal: ''
+                    background_color: 0.2, 0.2, 0.2, 1  # Темно-серый
+                Button:
+                    background_normal: ''
+                    background_color: 1, 1, 1, 1  # Белый
+                Button:
+                    background_normal: ''
+                    background_color: 0.4, 0.2, 0, 1  # Темно-коричневый
         PaintWidget:
             id: paint_widget
     BoxLayout:
@@ -79,6 +130,9 @@ BoxLayout:
 class PaintWidget(Widget):
     def __init__(self, **kwargs):
         super(PaintWidget, self).__init__(**kwargs)
+        self.line_width = 2
+        self.color = (0, 0, 0, 1)
+        self.eraser_mode = False
         self.update_canvas()
 
     def update_canvas(self):
@@ -91,6 +145,17 @@ class PaintWidget(Widget):
     def update_canvas_properties(self, instance, value):
         self.rect.size = self.size
         self.rect.pos = self.pos
+
+    def switch_to_brush(self):
+        self.eraser_mode = False
+        self.color = (0, 0, 0, 1)
+
+    def switch_to_eraser(self):
+        self.eraser_mode = True
+        self.color = (1, 1, 1, 1)
+
+    def clear_canvas(self):
+        self.update_canvas()
 
 class PaintApp(App):
     def build(self):
